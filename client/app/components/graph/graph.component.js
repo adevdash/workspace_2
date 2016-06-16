1b5f7ac36177a08e61965ab0e5d1f8281fe67f64
jetrax12 <jetrax12@gmail.com>
2016-06-16 12:46:44 -0400

    Put the AngularJSVG version of the Les Mis character force graph thingy. Looks naas

--- client/app/components/graph/graph.component.js
+++ client/app/components/graph/graph.component.js
@@ -4,11 +4,52 @@ angular.module('graph')
   .component('graph', {
     templateUrl: 'app/components/graph/graph.template.html',
     bindings: { message: '<' },
-    controller: function($http, $scope, socket, Auth, NodeDataFactory, NodeFilterFilter){//, getNodeData) {
+    controller: function($http, $scope, socket, Auth, NodeDataFactory){//, getNodeData) {
 
       this.network = NodeDataFactory.net;
       this.nodes = NodeDataFactory.restricted_node_list;
-      
+
+
+      // In the future it may be better to rework it from
+      // the ground up with purely AngularJS and SVG,
+      // as is explored at
+      // alexandros.resin.io/angular-d3-svg/
+      //
+
+      // Try to do the force-directed graph using AngularJSVG
+      $scope.width = 500;
+      $scope.height = 500;
+      var color = d3.scale.category20()
+      var force = d3.layout.force()
+        .charge(-120)
+        .linkDistance(30)
+        .size([$scope.width, $scope.height]);
+
+      $http.get('assets/miserables.json').then(
+      function(graph) {
+        //console.log(graph);
+        $scope.nodes = graph.data.nodes;
+        $scope.links = graph.data.links;
+
+        for(var i=0; i < $scope.links.length ; i++){
+          $scope.links[i].strokeWidth = Math.round(Math.sqrt($scope.links[i].value))
+        }
+
+        for(var i=0; i < $scope.nodes.length ; i++){
+          $scope.nodes[i].color = color($scope.nodes[i].group)
+        }
+
+        force
+          .nodes($scope.nodes)
+          .links($scope.links)
+          .on("tick", function(){$scope.$apply()})
+          .start();
+      }, err => {
+        console.log('Error retrieving JSON');
+        console.log(err.data);
+      });
+
+
 
 
       // Alright, let's see what we need here:
--- client/app/nodeFilter/nodeFilter.filter.js
+++ client/app/nodeFilter/nodeFilter.filter.js
@@ -4,6 +4,7 @@ angular.module('NodeFilterModule', [])
   .filter('NodeFilter', function () {
     return function (nodes, allowed_nodes) {
       var result_nodes = [];
+      console.log(allowed_nodes);
       for(var i in nodes){
         if(allowed_nodes.indexOf(nodes[i]._id) !== -1){
           result_nodes.push(nodes[i]);
